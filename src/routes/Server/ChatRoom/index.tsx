022d9cf571eca5fe6974684998ce6b5f79ca38fe
Bow3n <pioneerwbw@gmail.com>
2018-10-25 20:38:47 +0800

    WebSocket

--- src/routes/Server/ChatRoom/index.tsx
+++ src/routes/Server/ChatRoom/index.tsx
@@ -1,22 +1,57 @@
 import React, { Component } from "react";
+import { Button } from "antd";
 
 export class ChatRoom extends Component {
-  componentDidMount() {
-    const ws = new WebSocket("ws://127.0.0.1:4810/websocket");
-    ws.onopen = this.onOpen;
+  state = {
+    messages: [""]
+  };
+  ws: WebSocket;
+  // componentDidMount() {}
 
-    ws.onerror = evt => {
-      console.log(evt);
-    };
-    ws.send("hello");
-  }
+  openWebSocket = () => {
+    this.ws = new WebSocket("ws://localhost:4810");
+
+    this.ws.onopen = this.onOpen;
+    this.ws.onerror = this.onError;
+    this.ws.onmessage = this.onMessage;
+  };
 
   onOpen = () => {
+    this.ws.send("Hello websocket!");
     console.log("connected");
+  };
+  onError = evt => {
+    console.log(evt);
+  };
+  onMessage = event => {
+    console.log("Received data: " + event.data);
+    const messages = this.state.messages;
+    messages.push(event.data);
+    this.setState({ messages });
+  };
+
+  sendLove = () => {
+    if (this.ws.readyState) {
+      this.ws.send("Hello my love!");
+    }
   };
 
   render() {
-    return <div>ChatRoom</div>;
+    return (
+      <div>
+        <div>
+          {this.state.messages.map(m => (
+            <p key={m}>{m}</p>
+          ))}
+        </div>
+        <div>
+          <Button onClick={this.openWebSocket}>Test Connection</Button>
+        </div>
+        <div>
+          <Button onClick={this.sendLove}>Send My Love</Button>
+        </div>
+      </div>
+    );
   }
 }
 
